e9a512250f6f8e0562fab689dcab14206a2703b2
Valentina Armenise <valentina.armenise@gmail.com>
2014-04-29 12:51:43 +0200

    gradle unit test task

---
+++ app/src/test/java/com.valentina.converter_metals_tests/TestApp.java
@@ -0,0 +1,14 @@
+package com.valentina.converter_metals.app.tests;
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by valentinaarmenise on 16/04/2014.
+ */
+public class TestApp extends InstrumentationTestCase {
+
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 1;
+        assertEquals(expected, reality);
+    }
+}
