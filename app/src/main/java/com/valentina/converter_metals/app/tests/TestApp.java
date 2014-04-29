3a82625e37a17176a882f5ebda83a195d84be5f4
Valentina Armenise <valentina.armenise@gmail.com>
2014-04-29 15:32:56 +0200

    gradle unit test task

--- app/src/main/java/com/valentina/converter_metals/app/tests/TestApp.java
+++ app/src/main/java/com/valentina/converter_metals/app/tests/TestApp.java
@@ -1,10 +1,10 @@
 package com.valentina.converter_metals.app.tests;
-import android.test.InstrumentationTestCase;
+import android.test.AndroidTestCase;
 
 /**
  * Created by valentinaarmenise on 16/04/2014.
  */
-public class TestApp extends InstrumentationTestCase {
+public class TestApp extends AndroidTestCase {
 
     public void test() throws Exception {
         final int expected = 1;
